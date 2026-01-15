391be55a852ba8c1c2a4188e24572ec3062dec2e
alex241262 <alexandreferreira168@gmail.com>
2026-01-15 10:40:58 -0400

    comentado

--- br.com.projeto.VIEW/FrmVenda.cs
+++ br.com.projeto.VIEW/FrmVenda.cs
@@ -98,7 +98,7 @@ namespace Porjeto_Controle_Vendas.br.com.projeto.VIEW
             //verificar se foi apertado o entrer, pois nossa ação só pode ser executado depois dele
             if(e.KeyChar == 13)
             {
-                //alterao 15/01/2026
+                //alterao 15/01/2026 agoraaaaaaaaaaa
                 
                     cliente = clientedao.RetornaClientePorCpfCnpj(txtCNPJCPF.Text);
                 if (cliente != null)
